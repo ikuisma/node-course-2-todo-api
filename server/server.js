071f2e8d1ddb9d2492d38ed33c540e0401443c3d
ikuisma <ikuisma@Ilkkas-MacBook-Pro.local>
2017-05-14 11:34:51 +0300

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -5,6 +5,8 @@ var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/User');
 
+var {ObjectID} = require('mongodb');
+
 var app = express();
 
 app.use(bodyParser.json());
@@ -27,6 +29,22 @@ app.get('/todos', (req, res) => {
     res.status(400).send(e);
   });
 });
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  if (! ObjectID.isValid(id)) {
+    return res.status(400).send();
+  }
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+    res.status(200).send({todo});
+  }).catch((e) => {
+    res.status(404).send();
+  });
+});
+
 
 app.listen(3000, () => {
   console.log('Started on port 3000.');
