44c66d6d06fc7fc8a5fa25a13a9dbe0bb7f0633d
ikuisma <ikuisma@Ilkkas-MacBook-Pro.local>
2017-05-14 14:54:30 +0300

    Added tests for PATH /todos:id route.

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -11,6 +11,8 @@ const todos = [{
   }, {
     text: 'Second test todo',
     _id: new ObjectID(),
+    completed: true,
+    completedAt: 333
   }];
 
 beforeEach((done) => {
@@ -99,7 +101,7 @@ describe('GET /todos:id', () => {
   });
 });
 
-describe('DELETE /todos/:id', () => {
+describe('DELETE /todos:id', () => {
 
   it('should remove a todo', (done) => {
     var hexID = todos[1]._id.toHexString();
@@ -132,6 +134,46 @@ describe('DELETE /todos/:id', () => {
     request(app)
       .delete('/todos/123')
       .expect(404)
+      .end(done);
+  });
+
+});
+
+describe('PATCH /todos:id', () => {
+
+  it('should update the todo', (done) => {
+    var id = todos[0]._id.toHexString();
+    var text = 'This should be the new text'
+    request(app)
+      .patch(`/todos/${id}`)
+      .send({
+        text: text,
+        completed: true
+      })
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo.completed).toBe(true);
+        expect(res.body.todo.text).toBe(text);
+        expect(res.body.todo.completedAt).toBeA('number');
+      })
+      .end(done);
+  });
+
+  it('should clear completedAt when todo is not completed.', (done) => {
+    var id = todos[1]._id.toHexString();
+    var text = "some new text"
+    request(app)
+      .patch(`/todos/${id}`)
+      .send({
+        text,
+        completed: false
+      })
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo.text).toBe(text);
+        expect(res.body.todo.completedAt).toNotExist();
+        expect(res.body.todo.completed).toBe(false);
+      })
       .end(done);
   });
 
