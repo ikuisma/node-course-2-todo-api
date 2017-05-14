83759e7544a8e210aceffb6db1f12f9d6ad811d2
ikuisma <ikuisma@Ilkkas-MacBook-Pro.local>
2017-05-14 12:30:46 +0300

    Fixed typo in import.

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ var bodyParser = require('body-parser');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
-var {User} = require('./models/User');
+var {User} = require('./models/user');
 
 var {ObjectID} = require('mongodb');
 
